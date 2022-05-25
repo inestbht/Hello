ccae44831fb127744bf6a577f073ddf3c5c7492b
Samuel Pena <inestbht@gmail.com>
2022-05-24 23:02:15 -0500

    Completed - Ch. 2.8 - Exercise 3 - Hello Project

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func sayHelloPressed(_ sender: UIButton) {
+        let language1 = "G'Day"
+        let language2 = "Ciao"
+        let language3 = "Privyet"
+        
+        if messageLabel.text == language1 {
+            messageLabel.text = language2
+        } else if messageLabel.text == language2 {
+            messageLabel.text = language3
+        } else {
+            messageLabel.text = language1
+        }
+        
+    }
+    
 
 }
 
